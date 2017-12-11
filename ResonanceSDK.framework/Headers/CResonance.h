1426caefdc5df8ff14a23e6b855e43d966d0fec1
Nik <nikshebeda@gmail.com>
2017-12-11 18:33:47 +0200

    Fixed SDK

--- ResonanceSDK.framework/Headers/CResonance.h
+++ ResonanceSDK.framework/Headers/CResonance.h
@@ -16,8 +16,6 @@
  */
 @interface CResonance : NSObject <NSCoding, NSCopying>
 
-+(instancetype) sharedInstance;
-
 @property (nonatomic, copy) NSString* appId;
 @property (nonatomic, copy) NSString* customerId;
 @property (nonatomic, copy) NSString* sessionId;
@@ -26,71 +24,119 @@
 @property (nonatomic, strong) ResonanceBaseSettings* settings;
 
 
-
+/**
+ This property shows the current working status of Resonance SDK. Set "NO" to switch off Resonance SDK.
+ */
 @property (nonatomic, assign) BOOL status;
 
+/**
+ Configure Resonance SDK
+ */
++(void) setup;
 
+
+/**
+ Returns Resonance SDK shared instance
+ 
+ @return shared instance
+ */
++(instancetype) sharedInstance;
+
+/**
+ Method for sending log event actions
+ 
+ @param event log event name
+ @param items array of log objects
+ */
 -(void) logEvent:(NSString *) event
-		  params:(NSDictionary *) params
-	  completion:(void (^)(NSError* error, NSDictionary* schemes)) completion;
-
--(void) logEvent:(NSString *) event
-		   items:(NSArray *) items;
-
--(void) logSyncApiCallEvent:(NSString*) requestType
-                      event:(NSString *) event
-           items:(NSArray *) items
-         completion:(void (^)(NSError* error, NSString* string)) completion;
-
--(void) logAsyncApiCallWithCallback:(NSString *) event
-                      items:(NSArray *) items
-                 completion:(void (^)(NSError* error, NSString* string)) completion;
-
-
--(void) logEventSendUrl: (NSString *) event
-              urlString: (NSString *) url
-                  links:(NSArray *) links
            items:(NSArray *) items;
 
 -(void) logEvent:(NSString *) event
-		   items:(NSArray *) items
-		   links:(NSArray *) links
-requestRecommendations:(NSArray *) recommendations
-	  completion:(void (^)(NSError* error, NSDictionary* schemes)) completion;
+          params:(NSDictionary *) params
+      completion:(void (^)(NSError* error, NSDictionary* schemes)) completion;
 
--(void) logActionSyncApiCallEvent: (NSString* ) requestType
-            event:(NSString *) event
+
+/**
+  Method for sending log event actions with additinal parameters
+
+ @param event log event name
+ @param url log urls
+ @param links array of log links
+ @param items array of log objects
+ */
+-(void) logEventSendUrl: (NSString *) event
+              urlString: (NSString *) url
+                  links:(NSArray *) links
+                  items:(NSArray *) items;
+
+-(void) logEvent:(NSString *) event
            items:(NSArray *) items
            links:(NSArray *) links
 requestRecommendations:(NSArray *) recommendations
-      completion:(void (^)(NSError* error, NSString* string)) completion;
+      completion:(void (^)(NSError* error, NSDictionary* schemes)) completion;
+
+
+/**
+ Method for sending log event actions with additinal parameters
+
+
+ @param requestType type of request
+ @param event log event name
+ @param items array of log objects
+ @param links array of log links
+ @param completion handler of event status
+ */
+-(void) logActionApiCallEvent: (NSString* ) requestType
+                            event:(NSString *) event
+                            items:(NSArray *) items
+                            links:(NSArray *) links
+                       completion:(void (^)(NSError* error, NSString* string)) completion;
+
+//-(void) logSyncApiCallEvent:(NSString*) requestType
+//                      event:(NSString *) event
+//                      items:(NSArray *) items
+//                 completion:(void (^)(NSError* error, NSString* string)) completion;
+
+-(void) logAsyncApiCallWithCallback:(NSString *) event
+                              items:(NSArray *) items
+                         completion:(void (^)(NSError* error, NSString* string)) completion;
 
 
 -(void) logActionAsyncApiCallWithCallback:(NSString *) event
-                            items:(NSArray *) items
-                            links:(NSArray *) links
-           requestRecommendations:(NSArray *) recommendations
+                                    items:(NSArray *) items
+                                    links:(NSArray *) links
+                   requestRecommendations:(NSArray *) recommendations
                                completion:(void (^)(NSError* error, NSString* string)) completion;
 
 -(void) logTransactionEvent:(NSString *) event
-			  transactionId:(NSString *) transactionId
-					  items:(NSArray *) items;
+              transactionId:(NSString *) transactionId
+                      items:(NSArray *) items;
 
+
+/**
+   Method for sending log event for transaction actions 
+
+ @param event log event name
+ @param transactionId log transaction ID
+ @param transactionTotal Transaction total sum
+ @param items array of log objects
+ @param completion handler of event status
+ */
 -(void) logTransactionEvent:(NSString *) event
-			  transactionId:(NSString *) transactionId
-		   transactionTotal:(NSNumber *) transactionTotal
-					  items:(NSArray *) items
+              transactionId:(NSString *) transactionId
+           transactionTotal:(NSNumber *) transactionTotal
+                      items:(NSArray *) items
                  completion:(void (^)(NSError* error, NSString* string)) completion;
 
 -(void) logTransactionEvent:(NSString *) event
-			  transactionId:(NSString *) transactionId
-		   transactionTotal:(NSNumber *) transactionTotal
-					  items:(NSArray *) items
-					  links:(NSArray *) links
-	 requestRecommendations:(NSArray *) recommendations
-				 completion:(void (^)(NSError* error, NSDictionary* schemes)) completion;
+              transactionId:(NSString *) transactionId
+           transactionTotal:(NSNumber *) transactionTotal
+                      items:(NSArray *) items
+                      links:(NSArray *) links
+     requestRecommendations:(NSArray *) recommendations
+                 completion:(void (^)(NSError* error, NSDictionary* schemes)) completion;
+
 
 -(void) getRecomendations: (void (^)(NSError *, NSString *)) completion;
 -(void) setupSettings;
-+(void) setup;
 @end
